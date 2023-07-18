e2410e29d479ff05aa4d60a1b7b872e5b43360f1
rayyanahmed021 <68270622+rayyanahmed021@users.noreply.github.com>
2023-07-18 12:13:52 -0700

    Update index.py

--- index.py
+++ index.py
@@ -11,20 +11,13 @@ from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
-CORS(app, support_credentials=True)
+# CORS(app, support_credentials=True)
 
 movies = pd.read_csv('tmdb_5000_movies.csv')
 credits = pd.read_csv('tmdb_5000_credits.csv')
 credits1 = pd.read_csv('tmbd_5000_credit(2).csv')
 # credits_merged = credits.append(credits1,ignore_index=True)
 credits = pd.concat([credits, credits1],ignore_index=True )
-# combined_df = df_merged.reset_index(drop=True)
-
-# df_col_merged = pd.concat([credits, credits1], axis=1)
-
-# pd.concat(credits,credits1)
-# print(credits.columns, "credit1", credits1.columns)
-# print(df_merged)
 
 movies = movies.merge(credits,on='title')
 movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
@@ -86,9 +79,8 @@ cv = CountVectorizer(max_features=5000,stop_words='english')
 vector = cv.fit_transform(new['tags']).toarray()
 
 similarity = cosine_similarity(vector)
-new[new['title'] == 'The Lego Movie'].index[0]
+
 def recommend(movie):
-    print("asdadaa")
     index = new[new['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])),reverse=True,key = lambda x: x[1])
     recommended_movie_names = []
@@ -99,84 +91,10 @@ def recommend(movie):
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
     return recommended_movie_names
-# recommend('Gandhi')
-# pickle.dump(new,open('movie_list.pkl','wb'))
-# pickle.dump(similarity,open('similarity.pkl','wb'))
-
-
-
-# movies = pickle.load(open('/Users/rayyanahmed/Desktop/movie_recommender/movie_list.pkl','rb'))
-# similarity = pickle.load(open('/Users/rayyanahmed/Desktop/movie_recommender/similarity.pkl','rb'))
 
 @app.route('/data')
-@cross_origin(origin='*')
 def test():
     return recommend('Pan')
-    # return f"{request.args}"
-    # print(request.args)
-    # return recommend("Pan")
-    # response_body = {
-    #     "name": "Nagato",
-    #     "about" :"Hello! I'm a full stack developer that loves python and javascript"
-    # }
-    # return response_body
-
-
-@app.route('/recom')
-def test2():
-    recommended_movie_names,recommended_movie_posters = recommend("spider")
-    # return f"{movies}"
-    return recommended_movie_names
-
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
-
-# def recommend(movie):
-#     index = movies[movies['title'] == movie].index
-#     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-#     recommended_movie_names = []
-#     recommended_movie_posters = []
-#     for i in distances[1:6]:
-#         # fetch the movie poster
-#         movie_id = movies.iloc[i[0]].movie_id
-#         recommended_movie_posters.append(fetch_poster(movie_id))
-#         recommended_movie_names.append(movies.iloc[i[0]].title)
-
-#     return recommended_movie_names,recommended_movie_posters
-
-
-st.header('Movie Recommender System')
-
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
-
-# if st.button('Show Recommendation'):
-    # recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    # col1, col2, col3, col4, col5 = st.beta_columns(5)
-    # with col1:
-    #     st.text(recommended_movie_names[0])
-    #     st.image(recommended_movie_posters[0])
-    # with col2:
-    #     st.text(recommended_movie_names[1])
-    #     st.image(recommended_movie_posters[1])
-
-    # with col3:
-    #     st.text(recommended_movie_names[2])
-    #     st.image(recommended_movie_posters[2])
-    # with col4:
-    #     st.text(recommended_movie_names[3])
-    #     st.image(recommended_movie_posters[3])
-    # with col5:
-    #     st.text(recommended_movie_names[4])
-    #     st.image(recommended_movie_posters[4])
 
 
 
